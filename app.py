2992e73ed062d34f1083146ca48031d69d49a3ec
DuncanByrneUT <duncanbyrne7254@gmail.com>
2025-01-13 12:11:27 -0500

    loaded the csv file and printed the first few erows of the dataset and checked for the missing values. Columns are printing in the terminal

--- app.py
+++ app.py
@@ -5,3 +5,15 @@ import seaborn as sns
 
 
 # load our csv file
+df = pd.read_csv('CSV/archivenaturaldisaster/us_disaster_declarations.csv')
+
+# displaying the first few rowa  to get an understanding for the data structure
+print(df.head())
+
+#info about dataset
+print(df.info())
+
+# checking for msiising values in dataset
+
+print(df.isnull().sum())
+
